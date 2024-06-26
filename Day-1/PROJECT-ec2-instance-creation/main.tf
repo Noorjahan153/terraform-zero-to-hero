58659be3ac89af03b5cc06f36cd413fb098a919c
Noorjahan <90750848+Noorjahan153@users.noreply.github.com>
2024-06-26 12:22:59 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -6,5 +6,5 @@ resource "aws_instance" "example" {
     ami = "ami-0f58b397bc5c1f2e8"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
     subnet_id = "subnet-080041aa5625b631c"
-    Key_name = "aws_login"
+    Key_name = "my-vpc"
 }
